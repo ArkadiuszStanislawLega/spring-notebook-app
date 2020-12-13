c808e94d31f765b6b310439c53f31c68a7ff1347
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-12-13 09:27:43 +0100

    Initial

---
+++ src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/SpringNotebookAppApplication.java
@@ -0,0 +1,13 @@
+package pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNotebookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNotebookAppApplication.class, args);
+	}
+
+}
