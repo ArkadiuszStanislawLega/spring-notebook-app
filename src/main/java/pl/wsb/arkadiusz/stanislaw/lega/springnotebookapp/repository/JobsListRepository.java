a30cebe776a28705012597533902ffa337051bc0
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-12-18 10:00:32 +0100

    Added jobs list repository.

---
+++ src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/repository/JobsListRepository.java
@@ -0,0 +1,6 @@
+package pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobsListRepository extends JpaRepository<JpaRepository, Integer> {
+}
