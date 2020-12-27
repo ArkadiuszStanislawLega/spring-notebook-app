9eaecd4e5c65ad820bc2cd77ccf7bd61b33a8ff0
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-12-27 16:41:58 +0100

    Added job repository.

---
+++ src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.model.Job;
+import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.model.JobsList;
+
+import java.util.List;
+
+public interface JobRepository extends CrudRepository<Job, Integer> {
+    List<Job> findJobsByOwner(String owner);
+
+}
