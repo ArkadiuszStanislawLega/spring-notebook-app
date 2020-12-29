384f7f416e3001f393c835261a110976108adb36
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-12-29 08:44:42 +0100

    Update url - added urls details.

--- src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/stat/url.java
+++ src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/stat/url.java
@@ -13,17 +13,18 @@ public class url {
     private static final String HOME = "/home";
     private static final String NEW = "/new";
     private static final String SAVE = "/save";
+    private static final String DETAILS = "/details";
     private static final String EDIT = "/edit";
     private static final String SAVE_UPDATE = "/saveUpdate";
     private static final String DELETE=  "/delete";
     private static final String CONFIRM_DELETE = "/confirmDelete";
-
 
     public static final String JOBS_LIST_PAGE = "/jobsList";
     public static final String JOBS_LIST_PAGE_ACCESS_ALL = JOBS_LIST_PAGE + CHILDREN;
     public static final String JOBS_LIST_HOME_PAGE = JOBS_LIST_PAGE + HOME;
     public static final String JOBS_LIST_NEW_PAGE = JOBS_LIST_PAGE + NEW;
     public static final String JOBS_LIST_SAVE_PAGE = JOBS_LIST_PAGE + SAVE;
+    public static final String JOBS_LIST_DETAILS_PAGE = JOBS_LIST_PAGE + DETAILS;
     public static final String JOBS_LIST_EDIT_PAGE = JOBS_LIST_PAGE + EDIT;
     public static final String JOBS_LIST_SAVE_UPDATE_PAGE = JOBS_LIST_PAGE + SAVE_UPDATE;
     public static final String JOBS_LIST_DELETE_PAGE = JOBS_LIST_PAGE + DELETE;
@@ -34,6 +35,7 @@ public class url {
     public static final String JOB_HOME_PAGE = JOB_PAGE + HOME;
     public static final String JOB_NEW_PAGE = JOB_PAGE + NEW;
     public static final String JOB_SAVE_PAGE = JOB_PAGE + SAVE;
+    public static final String JOB_DETAILS_PAGE = JOB_PAGE + DETAILS;
     public static final String JOB_EDIT_PAGE = JOB_PAGE + EDIT;
     public static final String JOB_SAVE_UPDATE_PAGE = JOB_PAGE + SAVE_UPDATE;
     public static final String JOB_DELETE_PAGE = JOB_PAGE + DELETE;
