8cf16421224bda035254fd300a78880dd6863cd3
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-12-30 17:33:35 +0100

    Update job controller - added exceptions.

--- src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/controller/JobController.java
+++ src/main/java/pl/wsb/arkadiusz/stanislaw/lega/springnotebookapp/controller/JobController.java
@@ -7,14 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.comparators.JobComparatorByCreateDate;
-import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.comparators.JobListsComparatorsByCreateDate;
+import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.exceptions.NotFoundException;
+import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.exceptions.UnauthorizedException;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.model.Job;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.model.JobsList;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.model.User;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.service.JobService;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.service.JobsListService;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.service.UserService;
+import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.statics.Messages;
 import pl.wsb.arkadiusz.stanislaw.lega.springnotebookapp.statics.url;
 
 import java.util.*;
@@ -60,72 +61,128 @@ public class JobController {
     /**
      * Save created job in database.
      * Update edited date parent jobs list.
+     *
      * @param job Created new job.
      * @return Redirect to parent job list page.
      */
     @RequestMapping(value = url.JOB_SAVE_PAGE, method = {RequestMethod.POST, RequestMethod.PUT})
     public String save(@ModelAttribute("job") Job job) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User userLoggedIn = ownerService.findUserByUserName(auth.getName());
-        User jobParentOwner = this.parents.get(userLoggedIn.getId()).getOwner();
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            User userLoggedIn = ownerService.findUserByUserName(auth.getName());
+            User jobParentOwner = this.parents.get(userLoggedIn.getId()).getOwner();
 
-        if (userLoggedIn.getId() == jobParentOwner.getId()) {
-            job.setParent(this.parents.get(userLoggedIn.getId()));
-            job.setCreated(new Date());
-            job.setEdited(new Date());
-            jobService.saveJob(job);
+            if (jobParentOwner == null)
+                throw new NotFoundException(url.JOBS_LIST_DETAILS_PAGE + " -> " + Messages.ERROR_MESSAGE_NOT_FOUND);
 
-            //Update parent edited date.
-            JobsList dbJobList = parentService.find(job.getParent().getId());
-            dbJobList.setEdited(new Date());
-            parentService.saveJobsList(dbJobList);
+            if (userLoggedIn.getId() == jobParentOwner.getId()) {
+                job.setParent(this.parents.get(userLoggedIn.getId()));
+                job.setCreated(new Date());
+                job.setEdited(new Date());
+                jobService.saveJob(job);
 
-            this.parents.remove(userLoggedIn.getId());
+                //Update parent edited date.
+                JobsList dbJobList = parentService.find(job.getParent().getId());
+                dbJobList.setEdited(new Date());
+                parentService.saveJobsList(dbJobList);
+
+                this.parents.remove(userLoggedIn.getId());
+                return "redirect:" + url.JOBS_LIST_DETAILS_PAGE + "/" + job.getParent().getId();
+            }
+            else{
+                throw new UnauthorizedException(url.JOBS_LIST_DETAILS_PAGE + " -> " + Messages.ERROR_MESSAGE_UNAUTHORIZED);
+            }
+
+        } catch (UnauthorizedException ex) {
+            return url.ERROR_UNAUTHORIZED;
+        } catch (NotFoundException ex) {
+            return url.ERROR_NOT_FOUND;
         }
-        return "redirect:" + url.JOBS_LIST_DETAILS_PAGE + "/" + job.getParent().getId();
     }
 
     @GetMapping(value = url.JOB_EDIT_PAGE + "/{id}")
     public ModelAndView edit(@PathVariable(name = "id") int id) {
         ModelAndView modelAndView = new ModelAndView(url.JOB_EDIT_PAGE);
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User owner = ownerService.findUserByUserName(auth.getName());
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            User owner = ownerService.findUserByUserName(auth.getName());
 
-        Job job = jobService.find(id);
-        this.parents.put(owner.getId(), job.getParent());
+            Job job = jobService.find(id);
+            if (job == null)
+                throw new NotFoundException(url.JOB_EDIT_PAGE + "/" + id + "-> " + Messages.ERROR_MESSAGE_NOT_FOUND);
 
-        modelAndView.addObject("job", job);
-        return modelAndView;
+            if (owner.getId() == job.getParent().getOwner().getId()) {
+                this.parents.put(owner.getId(), job.getParent());
+                modelAndView.addObject("job", job);
+                return modelAndView;
+            } else {
+                throw new UnauthorizedException(url.JOB_EDIT_PAGE + "/" + id + "-> " + Messages.ERROR_MESSAGE_UNAUTHORIZED);
+            }
+
+        } catch (UnauthorizedException ex) {
+            modelAndView.setViewName(url.ERROR_UNAUTHORIZED);
+            return modelAndView;
+        } catch (NotFoundException ex) {
+            modelAndView.setViewName(url.ERROR_NOT_FOUND);
+            return modelAndView;
+        }
     }
 
     @RequestMapping(value = url.JOB_SAVE_UPDATE_PAGE, method = {RequestMethod.GET, RequestMethod.PUT})
     public String saveUpdate(@ModelAttribute("job") Job job) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User userLoggedIn = ownerService.findUserByUserName(auth.getName());
-        User jobParentOwner = this.parents.get(userLoggedIn.getId()).getOwner();
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            User userLoggedIn = ownerService.findUserByUserName(auth.getName());
+            User jobParentOwner = this.parents.get(userLoggedIn.getId()).getOwner();
 
-        if (userLoggedIn.getId() == jobParentOwner.getId()) {
-            job.setParent(this.parents.get(userLoggedIn.getId()));
-            job.getParent().setEdited(new Date());
-            jobService.saveJob(job);
+            if (jobParentOwner == null)
+                throw new NotFoundException(url.JOB_SAVE_UPDATE_PAGE + "-> jobParentOwner ->" + Messages.ERROR_MESSAGE_NOT_FOUND);
 
-            this.parents.remove(userLoggedIn.getId());
+            if (userLoggedIn.getId() == jobParentOwner.getId()) {
+                Date editedDate = new Date();
+                job.setParent(this.parents.get(userLoggedIn.getId()));
+                job.getParent().setEdited(editedDate);
+                job.setEdited(editedDate);
+                jobService.saveJob(job);
+
+                this.parents.remove(userLoggedIn.getId());
+            } else {
+                throw new UnauthorizedException(url.JOB_SAVE_UPDATE_PAGE + "-> " + Messages.ERROR_MESSAGE_UNAUTHORIZED);
+            }
+
+            return "redirect:" + url.JOB_HOME_PAGE;
+
+        } catch (UnauthorizedException ex) {
+            return url.ERROR_UNAUTHORIZED;
+        } catch (NotFoundException ex) {
+            return url.ERROR_NOT_FOUND;
         }
-
-        return "redirect:" + url.JOB_HOME_PAGE;
     }
 
     @RequestMapping(value = url.JOB_DELETE_PAGE + "/{id}")
     public String delete(@PathVariable(name = "id") int id) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = ownerService.findUserByUserName(auth.getName());
-        Job job = jobService.find(id);
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            User user = ownerService.findUserByUserName(auth.getName());
+            Job job = jobService.find(id);
 
-        if (user.getId() == job.getParent().getOwner().getId()) {
-            job.getParent().setEdited(new Date());
-            jobService.removeJob(job);
+            if (job == null)
+                throw new NotFoundException(url.JOB_DELETE_PAGE + "/" + id + "-> " + Messages.ERROR_MESSAGE_NOT_FOUND);
+
+            if (user.getId() == job.getParent().getOwner().getId()) {
+                job.getParent().setEdited(new Date());
+                jobService.removeJob(job);
+            } else {
+                throw new UnauthorizedException(url.JOB_DELETE_PAGE + "/" + id + "-> " + Messages.ERROR_MESSAGE_UNAUTHORIZED);
+            }
+
+            return "redirect:" + url.JOB_HOME_PAGE;
+
+        } catch (UnauthorizedException ex) {
+            return url.ERROR_UNAUTHORIZED;
+        } catch (
+                NotFoundException ex) {
+            return url.ERROR_NOT_FOUND;
         }
-
-        return "redirect:" + url.JOB_HOME_PAGE;
     }
 }
